c132380ea48e899fd5ea6b29198659fd8c8ae751
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-03 18:51:11 -0500

    Calculated Age biased on year
    Learning about prompts

---
+++ Day 4/js/day4Promts.js
@@ -0,0 +1,22 @@
+/*
+Michael Giacchetta
+Day 4 Prompts
+WPF 1411
+11-03-2014
+ */
+
+//alert("Testing 1,2,3");
+
+// Ask user for input
+
+//prompt() - asks the user to type something in
+var yearBorn = prompt("Enter your year of birth:");
+console.log(yearBorn);
+
+//Figure out how old we are
+var age = 2014 - yearBorn;
+console.log("You are "+age+" years old.");
+
+
+
+
