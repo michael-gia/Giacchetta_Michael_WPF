049277b456f08eff64cf879c902bd346e5d37891
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-04 00:07:51 -0500

    Finished log in Wacky
    Finished my final console log in wacky

--- Expressions_Assignment/Expression_Wacky/js/wackyExpressions.js
+++ Expressions_Assignment/Expression_Wacky/js/wackyExpressions.js
@@ -10,11 +10,17 @@ alert("Testing 1,2,3");
 
 
 //If teletubbies are trying to hug every person in the world.
- //each Teletubbie can only cover 26 miles each
+ //each Teletubbie can only cover 26 miles each, those are tellie Miles
 
 
  var numberOfTelies = 80;
  var numberOfTellieMiles = 26;
+
+ //How many miles can the 80 tellies cover to hug everyone
+
+ var hugradious = numberOfTelies * numberOfTellieMiles;
+
+ console.log("When the Teletubbies move into America on their hug rampage they will be able to cover "+hugradious+" miles, hugging everything in sight")
 
 
 
