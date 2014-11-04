403f267a80782f59061d2e142ad363777ba04586
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-03 19:08:18 -0500

    Finished Prompting

--- Day 4/js/day4Promts.js
+++ Day 4/js/day4Promts.js
@@ -19,4 +19,26 @@ console.log("You are "+age+" years old.");
 
 
 
+//Calculate the area of a rectangle
+//area= legnth * width
+
+var length = prompt(" We are going to calculate the area of a rectangle.\n Please enter the length of your rectangle:");
+var width = prompt ("Please enter the width of your rectangle:")
+
+//calculate the actual area
+var area = length * width;
+
+//Create a result variable and then console log that
+var results = "When your rectangle has the length of "+length+" and the width of "+width+" has an area of "+area+".";
+
+console.log(results)
+alert(results)
+
+//console the area
+//console.log("The area of your rectangle is "+area+".");
+console.log("When your rectangle has the length of "+length+" and the width of "+width+" has an area of "+area+".")
+
+//alert the user with the same information
+alert("When your rectangle has the length of "+length+" and the width of "+width+" has an area of "+area+".")
+
 
