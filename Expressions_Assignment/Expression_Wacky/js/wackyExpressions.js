1c797799e1a0472a0da57ebcd1cec34aa92ec84d
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-04 00:01:35 -0500

    New Directions
    Decided to go in a new direction for Wacky

--- Expressions_Assignment/Expression_Wacky/js/wackyExpressions.js
+++ Expressions_Assignment/Expression_Wacky/js/wackyExpressions.js
@@ -10,17 +10,14 @@ alert("Testing 1,2,3");
 
 
 //If teletubbies are trying to hug every person in the world.
+ //each Teletubbie can only cover 26 miles each
 
 
  var numberOfTelies = 80;
- var numberOfPeople = 26000000000;
- var numberOfContinents = 7;
+ var numberOfTellieMiles = 26;
 
- var peoplePerContinent = numberOfPeople / numberOfContinents;
 
- //How do the Teletubbies need to disburse to hug everyone in the world?
 
- 
 
 
 
