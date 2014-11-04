7df48f695dcf688af2447d17fe85004a92091a7d
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-03 23:03:43 -0500

    Added some Cost Equations
    Added some cost equations and added a comment about what the next step
    is

--- Expressions_Assignment/Expressions_Industry/js/industryExpressions.js
+++ Expressions_Assignment/Expressions_Industry/js/industryExpressions.js
@@ -11,12 +11,18 @@ WPF 1411
 //For this I am going to create an invoice for a photographer covering a a conferance.
 
  var costPerDay =150
+ var costPerPicture =.25 //plus $1 digital storage fee
  var photosTaken =[440, 200, 500,];
  var totalPhotos = photosTaken[0]+photosTaken[1]+photosTaken[2];
 
 
 
-var photoFees = photosTaken.length * costPerDay; 
+var costPerDayTotal = photosTaken.length * costPerDay;
+ var costPerPictureTotal = totalPhotos * costPerPicture;
+
+ //Lets say that two companies hosted the conference and are splitting the totaly bill for the photographer.
+
+ 
 
 
 
