e0d6e2175a6134a34357eb62825048dda054ebff
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-03 21:14:26 -0500

    Created All Files
    Created, and tested all the files and directories for the Expressions
    assignment.

---
+++ Expressions_Assignment/Expression_Wacky/js/wackyExpressions.js
@@ -0,0 +1,10 @@
+
+ /*
+Michael Giacchetta
+Wacky Expressions
+WPF 1411
+11-03-2014
+ */
+
+alert("Testing 1,2,3");
+
---
+++ Expressions_Assignment/Expressions_Personal/js/personalExpressions.js
@@ -0,0 +1,12 @@
+
+ /*
+Michael Giacchetta
+Personal Expressions
+WPF 1411
+11-03-2014
+ */
+
+//alert("Testing 1,2,3");
+
+
+
