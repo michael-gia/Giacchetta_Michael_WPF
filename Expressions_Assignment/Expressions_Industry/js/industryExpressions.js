a368a6c4150c77333d43a3c2dc28b1bb2fe66158
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-03 23:23:47 -0500

    Finished Industry
    I finished the industry expressions.

--- Expressions_Assignment/Expressions_Industry/js/industryExpressions.js
+++ Expressions_Assignment/Expressions_Industry/js/industryExpressions.js
@@ -11,7 +11,7 @@ WPF 1411
 //For this I am going to create an invoice for a photographer covering a a conferance.
 
  var costPerDay =150
- var costPerPicture =.25 //plus $1 digital storage fee
+ var costPerPicture =.25
  var photosTaken =[440, 200, 500,];
  var totalPhotos = photosTaken[0]+photosTaken[1]+photosTaken[2];
 
@@ -28,6 +28,8 @@ var costPerDayTotal = photosTaken.length * costPerDay;
  console.log("The conferance lasted "+photosTaken.length+" days. Over the course of that time "+totalPhotos+" photos were taken.\n"+
  "With the fee of $"+costPerDay+" per day, combined with the fee of "+costPerPicture+" cents per picture brings the total cost to $"+totalCost+".\n" +
  "Your company owes a total of $"+finalInvoice+".");
+
+ //With the two companies splitting the cost, the total should come out to $367.50
 
 
 
