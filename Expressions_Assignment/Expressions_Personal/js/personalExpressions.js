68a19d64143f95cd285f96abfe9d6595368d24a8
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-03 22:19:02 -0500

    Simplified again
    I turned two console logs into one and still have the same information
    displayed.

--- Expressions_Assignment/Expressions_Personal/js/personalExpressions.js
+++ Expressions_Assignment/Expressions_Personal/js/personalExpressions.js
@@ -18,17 +18,17 @@ WPF 1411
 
 
 
- var week1 =prompt("How many k-cups did you use in the first week of the month?");
- var week2 =prompt("How many k-cups did you use in the second week of the month?");
- var week3 =prompt("How many k-cups did you use in the third week of the month?");
- var week4 =prompt("How many k-cups did you use in the fourth week of the month?");
+ var week1 =prompt("How many k-cups did you use in the First week of the month?");
+ var week2 =prompt("How many k-cups did you use in the Second week of the month?");
+ var week3 =prompt("How many k-cups did you use in the Third week of the month?");
+ var week4 =prompt("How many k-cups did you use in the Fourth week of the month?");
 
 
  var totalCupsUsed = parseInt(week1)+parseInt(week2)+parseInt(week3)+parseInt(week4);
-
- console.log("From the "+kCupBoxs+" boxes of k-cups you bought at the beginning of the month, You used "+totalCupsUsed+" k-cups.");
  var cupsLeftOver = kCupsbought - totalCupsUsed;
- console.log("This leaves you with "+cupsLeftOver+" k-cups at the end of the month.");
+
+ console.log("From the "+kCupBoxs+" boxes of k-cups you bought at the beginning of the month, You used "+totalCupsUsed+" k-cups." +
+ "\n This leaves you with "+cupsLeftOver+" k-cups at the end of the month.");
 
 
 
