c69b1976159b5b9017fa79a2bede0f796a21cbb7
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-03 22:04:08 -0500

    Started the final equations for personal
    I am using var and math to determine the answer to my problem.

--- Expressions_Assignment/Expressions_Personal/js/personalExpressions.js
+++ Expressions_Assignment/Expressions_Personal/js/personalExpressions.js
@@ -10,8 +10,11 @@ WPF 1411
 
 //Lets calculate how many Kurieg K-cups a person used in a month and determine how many more we need to buy.
 //I will start by prompting the person with how many k-cup he/she uses for each week of the month.
-//How many boxes will I need to buy at the end of the month
- var kCupPerBox =(12);
+
+
+ var kCupBoxs =prompt("How many boxes of k-cups did you buy at the beginning of the month?");
+
+ var kCupsPerBox =kCupBoxs*12;
 
 
  var week1 =prompt("How many k-cups did you use in the first week of the month?");
@@ -20,10 +23,9 @@ WPF 1411
  var week4 =prompt("How many k-cups did you use in the fourth week of the month?");
 
 
- var totalUsed = parseInt(week1)+parseInt(week2)+parseInt(week3)+parseInt(week4);
+ var totalCupsUsed = parseInt(week1)+parseInt(week2)+parseInt(week3)+parseInt(week4);
 
- //console.log(totalUsed);
-
+ console.log("From the"+kCupBoxs+" boxes you bought at the beginning of the month, You used "+totalCupsUsed+" k-cups.");
 
 
 
