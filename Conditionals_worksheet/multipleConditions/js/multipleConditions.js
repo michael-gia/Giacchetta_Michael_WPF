e36f7bb648a6bd25f3426898eb009ce3c9ec7741
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-05 23:51:54 -0500

    Tested and refined code to it's simplest form.
    Tested and refined code to it's simplest form.

--- Conditionals_worksheet/multipleConditions/js/multipleConditions.js
+++ Conditionals_worksheet/multipleConditions/js/multipleConditions.js
@@ -9,7 +9,7 @@ WPF 1411
 //alert("Testing 1,2,3");
 
  var frontTirePressure =[32,32];
- var backTirePressure =[30,33];
+ var backTirePressure =[30,30];
 
  if ((frontTirePressure[0]==frontTirePressure[1]) && (backTirePressure[0]==backTirePressure[1])) {
   console.log("The tires pass pec!");
