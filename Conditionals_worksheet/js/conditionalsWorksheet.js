6e8bf9a99f1e1dea89f7b5b4346aef5e49761b5f
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-05 22:01:58 -0500

    Started Group 1 Problem
    Gallons of gas equation

--- Conditionals_worksheet/js/conditionalsWorksheet.js
+++ Conditionals_worksheet/js/conditionalsWorksheet.js
@@ -11,4 +11,15 @@ WPF 1411
 
 //Group1: Expressions & Conditionals
 
- 
+ var gasEfficency = 32
+ var tankGuageReading = .25
+ var gasCapacity = 16
+
+ var gallonsRemaining =  gasCapacity * tankGuageReading;
+
+
+
+ if (gallonsRemaining){
+
+
+ }
