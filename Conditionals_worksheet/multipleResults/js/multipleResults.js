3b5688c87cc17a3380dd6e4cbe2418b6f0ae9619
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-05 22:44:44 -0500

    Started group2
    Working on username and password varification

---
+++ Conditionals_worksheet/multipleResults/js/multipleResults.js
@@ -0,0 +1,25 @@
+
+ /*
+Michael Giacchetta
+Multiple Results
+WPF 1411
+11-05-2014
+ */
+
+//alert("Testing 1,2,3");
+
+ var username = prompt("Please Enter Username");
+ var password = prompt("Please Enter Password");
+ var correctName = "Hodor";
+ var correctPass = "8675309"
+
+if (username===correctName && username){
+  console.log("");
+
+}
+
+
+
+
+
+
