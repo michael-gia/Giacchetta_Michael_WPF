1a87c45e3e2ca3f82657b0b11bff5393bbbf6efe
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-05 23:51:07 -0500

    Fixed code
    Fixed code, trying to find the most efficient way to write the else.

--- Conditionals_worksheet/multipleConditions/js/multipleConditions.js
+++ Conditionals_worksheet/multipleConditions/js/multipleConditions.js
@@ -9,20 +9,20 @@ WPF 1411
 //alert("Testing 1,2,3");
 
  var frontTirePressure =[32,32];
- var backTirePressure =[30,35];
+ var backTirePressure =[30,33];
 
- if (frontTirePressure[0]=frontTirePressure[1] && (backTirePressure[0]=backTirePressure[1])) {
+ if ((frontTirePressure[0]==frontTirePressure[1]) && (backTirePressure[0]==backTirePressure[1])) {
   console.log("The tires pass pec!");
 
  }
 
- else (frontTirePressure[0] frontTirePressure[1]) || backTirePressure[0] != backTirePressure[1])  {
+
+ else if (frontTirePressure[0] !== frontTirePressure[1] || backTirePressure[0] !== backTirePressure[1])
+ {
 
   console.log("Get your tires checked out!");
 
  }
-
- //
 
 
 
