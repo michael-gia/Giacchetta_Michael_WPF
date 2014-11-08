d4eb4e47ec055e11ed7a3a883d085f26ed7e1f5e
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-07 20:41:12 -0500

    Finished For Loops
    Finished For Loops

--- Day 7/js/day7ForLoops.js
+++ Day 7/js/day7ForLoops.js
@@ -6,5 +6,31 @@ WPF 1411
 11-07-2014
  */
 
-alert("Testing 1,2,3");
+//alert("Testing 1,2,3");
+
+ //Basic For Loops
+  //for(initialization of counter; condition to test; increment of Change)
+  //i - stands for iteration
+
+ for( var i =0; i<20; i+=4){
+    console.log("i= "+i);
+
+ }
+
+
+ //Breaks
+
+   //Created a for loop but if the counter variable is 3, I want to stop the loop
+
+ for( var j =0; j<5; j++ ){
+
+    console.log("j= "+j);
+     if(j===3){
+      break; //This stops looks from cycling
+
+
+     }
+
+ }
+
 
