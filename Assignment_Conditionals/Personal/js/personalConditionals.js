2ecce019b58ad53937bc946a294682b11f08a1ef
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-09 16:06:25 -0500

    Troubleshoot Nesting in Personal
    Troubleshoot Nesting in Personal

--- Assignment_Conditionals/Personal/js/personalConditionals.js
+++ Assignment_Conditionals/Personal/js/personalConditionals.js
@@ -11,8 +11,20 @@ WPF 1411
  // Lets go kayaking!
  // But we only have 3 kayaks
 
- var numOfPeople = prompt("How many people are we hanging out with today?");
+
  var numOfKayaks = 3;
+ var weatherTemp = 75;
 
  //Is the weather nice enough to go kayaking?
+
+if (weatherTemp>=65){
+   console.log("It's nice enough to go kayacking!");
+  numOfPeople = prompt("How many people are we hanging out with today?");
+   if (numOfPeople>numOfKayaks){
+     console.log("Sorry we don't have enough kayaks.");
+   }
+
+} else {
+   console.log("Lets hang out at a coffee shop");
+}
 
