ed580cc3b1917cfcf0ba738a6038a7e78ba84ac0
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-09 13:45:11 -0500

    Created Files for Conditionals Assignment
    Created Files for Conditionals Assignment

---
+++ Assignment_Conditionals/Personal/js/personalConditionals.js
@@ -0,0 +1,10 @@
+
+ /*
+Michael Giacchetta
+Personal Conditionals
+WPF 1411
+11-09-2014
+ */
+
+alert("Testing 1,2,3");
+
---
+++ Assignment_Conditionals/Wacky/js/wackyConditionals.js
@@ -0,0 +1,10 @@
+
+ /*
+Michael Giacchetta
+Wacky Conditionals
+WPF 1411
+11-09-2014
+ */
+
+alert("Testing 1,2,3");
+
