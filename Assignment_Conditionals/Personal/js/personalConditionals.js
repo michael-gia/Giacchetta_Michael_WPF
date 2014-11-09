b033b7a36c1e687d744547e67e3c69f3a25ed3dc
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-09 17:21:42 -0500

    Added some more comments to Personal
    Added some more comments to Personal to make this more clear.

--- Assignment_Conditionals/Personal/js/personalConditionals.js
+++ Assignment_Conditionals/Personal/js/personalConditionals.js
@@ -11,29 +11,35 @@ WPF 1411
  // Lets go kayaking!
  // But we only have 3 kayaks
 
-
- var numOfKayaks = 3;
- var weatherTemp = prompt("What is the weather tempature today?");
-
  //Is the weather nice enough to go kayaking?
 
 
+ var numOfKayaks = 3;
+ var weatherTemp = prompt("What is the weather temp today?");
 
-if (weatherTemp>=65) {
+
+
+ if (weatherTemp>=65) {
  console.log("Lets go kayaking!");
 
  numberOfPeople = prompt("How many people are we hanging out with today?")
 
   if(numberOfPeople<=numOfKayaks){
     console.log("Sweet, we have enough kayaks for everyone!");
+
   } else {
    console.log("We don't have enough kayaks, lets play some frisbee golf!");
   }
+
+  //If we don't want to be outside than let grab a drink!
 
 } else {
  console.log("Coffee shop Time!");
 
  numberOfPeople = prompt("How many people are we hanging out with today?")
+  
+  //Maybe more of the group are beer people, lets ask them!
+
  beerorCoffee = prompt("How many people really want coffee right now?");
 
 
