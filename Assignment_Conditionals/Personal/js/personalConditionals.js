f87ff66745215bad6f870ca101c6aeaf51c9f8b2
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-09 15:56:20 -0500

    Added Variables to Personal
    Added Variables to Personal

--- Assignment_Conditionals/Personal/js/personalConditionals.js
+++ Assignment_Conditionals/Personal/js/personalConditionals.js
@@ -6,5 +6,13 @@ WPF 1411
 11-09-2014
  */
 
-alert("Testing 1,2,3");
+//alert("Testing 1,2,3");
+
+ // Lets go kayaking!
+ // But we only have 3 kayaks
+
+ var numOfPeople = prompt("How many people are we hanging out with today?");
+ var numOfKayaks = 3;
+
+ //Is the weather nice enough to go kayaking?
 
