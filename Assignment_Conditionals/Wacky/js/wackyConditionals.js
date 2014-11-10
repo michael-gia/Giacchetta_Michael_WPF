a411bba0999a79f956f32bc2c6ecf6fc2282b1af
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-10 15:30:13 -0500

    Fixed the issue with wacky
    Fixed the issue with wacky, I forgot to turn the prompt answer into
    numbers.

--- Assignment_Conditionals/Wacky/js/wackyConditionals.js
+++ Assignment_Conditionals/Wacky/js/wackyConditionals.js
@@ -12,17 +12,17 @@ WPF 1411
 var pencils = prompt("How many pencils do you have?");
  var apples = prompt("How many apples do you have");
 
- var pancakesOnRoof = pencils + apples;
+ var pancakesOnRoof = Number(pencils) + Number(apples);
 
  console.log("With "+pencils+" Pencils and "+apples+" Apples, how many pancakes will you have on the roof?");
 
  if (pancakesOnRoof>=20 && pancakesOnRoof <=100) {
   console.log("Purple because aliens don't wear hats.");
 
- } else if (pancakesOnRoof>20 && pancakesOnRoof>100) {
+ } else if (pancakesOnRoof>=20 && (pancakesOnRoof>100)) {
    console.log("Shfifty Fiave!");
 
-  } else {
+  } else if (pancakesOnRoof<19) {
    console.log("Pink because the wacky assignment made him as mad as a hatter!");
   }
 
