84bcc3de68c5f3c0877a2f9d1cedd67ede68e7ec
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-10 14:12:55 -0500

    Fixed how some console logs display in console.
    Fixed how some console logs display in console.

--- Assignment_Conditionals/Industry/js/industryConditionals.js
+++ Assignment_Conditionals/Industry/js/industryConditionals.js
@@ -10,7 +10,7 @@ WPF 1411
 
 // Car rental
  //Lets say a business is sending a manager on a business trip.
- //We can calculate out the cost and reimbursement
+ //We can calculate out the cost
 
 var travelDistance = prompt("How many miles will you be traveling");
 var budget = 600;
@@ -22,8 +22,12 @@ var costPerMile = .55;
 
  if (milageCost<=budget && travelDistance>800){
    console.log("Lets start the trip, but we are going pretty far.");
+  //I'm betting that a trip over 800 miles is going to take a while So I am calculating in lodging costs.
     var lodgingCost = prompt("How much is a Motel going to cost for a night?");
-     
+
+     if((remainingBudget-lodgingCost) <= 0){
+       console.log("We can't afford that motel, lets find something cheaper or consider sleeping in the car.");
+     }
 
 
  } else if(milageCost<=budget && travelDistance<800) {
