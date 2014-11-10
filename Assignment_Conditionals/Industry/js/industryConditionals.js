011fdc6c62a392420d7733135fc8e758e6ea8334
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-09 22:36:34 -0500

    Started Variables for Industry
    Started Variables for Industry

--- Assignment_Conditionals/Industry/js/industryConditionals.js
+++ Assignment_Conditionals/Industry/js/industryConditionals.js
@@ -8,6 +8,13 @@ WPF 1411
 
 //alert("Testing 1,2,3");
 
+// Car rental
+ //Lets say a business is sending a manager on a business trip.
+ //We can calculate out the cost and reimbursement
+
+var travelDistance = prompt("How many mile will you be traveling");
+var budget = 200;
+var costPerMile = .55;
 
 
 
