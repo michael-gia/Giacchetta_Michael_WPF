26eeaab170ac2521dbf40e28de960af079abf784
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-10 15:19:04 -0500

    Wrote variable and ifs for wacky
    Wrote variable and ifs for wacky, its not working. Trying to fix it.

--- Assignment_Conditionals/Wacky/js/wackyConditionals.js
+++ Assignment_Conditionals/Wacky/js/wackyConditionals.js
@@ -9,4 +9,20 @@ WPF 1411
 //alert("Testing 1,2,3");
 
 
+var pencils = prompt("How many pencils do you have?");
+ var apples = prompt("How many apples do you have");
+
+ var pancakesOnRoof = pencils + apples;
+
+ console.log("With "+pencils+" Pencils and "+apples+" Apples, how many pancakes will you have on the roof?");
+
+ if (pancakesOnRoof>=20 && pancakesOnRoof <=100) {
+  console.log("Purple because aliens don't wear hats.");
+
+ } else if (pancakesOnRoof>20 && pancakesOnRoof>100) {
+   console.log("Shfifty Fiave!");
+
+  } else {
+   console.log("Pink because the wacky assignment made him as mad as a hatter!");
+  }
 
