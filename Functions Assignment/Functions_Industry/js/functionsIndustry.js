e80c9752461ad5acc73827c406e3e6e685026c1f
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-14 22:35:34 -0500

    Finished the final console log for industry
    Finished the final console log for industry, added a .tofixed to my
    final result.

--- Functions Assignment/Functions_Industry/js/functionsIndustry.js
+++ Functions Assignment/Functions_Industry/js/functionsIndustry.js
@@ -11,34 +11,17 @@ WPF 1411
 //Trying to calculate how many tiles we will need to resurface the kitchen floor.
  // How much will it cost to get the tiles.
 
+
+//Here are the variables we will prompt the user for
  var floorArea =prompt("How many square feet is the kitchen?");
  var tileSpec = 2; //Square feet
  var costPerTile =prompt("How much does each time cost?");
 
+//Here is the funcation call and variable that uses the function
+ tileAmount()
+var results =tileAmount(floorArea,tileSpec,costPerTile);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ console.log("While redoing your kitchen floor that is "+floorArea+" Sq feet, you will need "+(floorArea/tileSpec)+" tiles.\nIf the cost per tile is "+costPerTile+" your total cost will be "+results.toFixed(2)+".");
 
 
 
@@ -58,7 +41,6 @@ WPF 1411
 
     var howMany = area/tilespec;
     var totalCost = howMany*cost;
-     return howMany;
      return totalCost;
 
  }
