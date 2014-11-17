9000e8847227f2722a7e4eea43f6e0aadd2cfdea
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-16 19:31:41 -0500

    Started Wacky Function
    Started Wacky Function. Hard coded function to start with and test,
    will change.

--- Functions Assignment/Functions_Wacky/js/functionsWacky.js
+++ Functions Assignment/Functions_Wacky/js/functionsWacky.js
@@ -17,19 +17,32 @@ WPF 1411
  // 51 - 75 is Scissors
 
 
+//Here are the amount of players
 
  var player1 =Math.random() *75;
  var player2 =Math.random() *75;
 
-console.log(player1);
+   console.log(player1);
+   console.log(player2);
 
 
 
- if(gameNum<=25){
-     console.log("You got Rock");
+
+//Here is the function that will be used for each player.
+function rockPaperScissors (r,p,s) {
 
 
+ if (player1 <= 25) {
+  console.log("You got Rock");
+
+ } else if (player1 > 25 && player1 <= 50) {
+  console.log("You got Paper");
+
+ } else if (player1 > 50 && player1 <= 75) {
+  console.log("You got Scissors");
  }
+
+}
 
 
 
