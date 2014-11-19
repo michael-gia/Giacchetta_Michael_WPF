7887129d50f4109d9dd38819b22019e4f0cc0e47
michael-gia <michaelgiacchetta@Michaels-MacBook-Pro-4.local>
2014-11-19 18:15:03 -0500

    Created Final Exam files and started variables
    Created Final Exam files and started variables

---
+++ finalExam/js/finalExam.js
@@ -0,0 +1,17 @@
+
+ /*
+Michael Giacchetta
+Final Exam
+WPF 1411
+11-19-2014
+ */
+
+//alert("Testing 1,2,3");
+
+ var itemCost =prompt("How much does Item #1 cost?");
+
+ (itemCost ==="" && itemCost ===isNaN(itemCost)) ? "Please only type in a number and do not leave blank";
+
+ console.log(itemCost);
+ 
+
